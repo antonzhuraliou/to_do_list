d3c39496941f7a28143cf49ef71b1d1c4ced17c4
Anton <anton.zhurauliou@gmail.com>
2025-03-05 08:35:55 +0100

    Connect DB and Form

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Task
+from .forms import TodoForm
 # Create your views here.
 
 def get_page(request):
-    return render(request, 'tasks/cur_tasks.html')
-
+    tasks = Task.objects.all()
+    form = TodoForm()
+    return render(request, 'tasks/cur_tasks.html', context = {'tasks': tasks, 'form': form})
 # def show_
