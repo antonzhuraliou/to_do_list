126707724d1043786d315ddc93635b6245f2b57f
Anton <anton.zhurauliou@gmail.com>
2025-03-06 10:05:50 +0100

    Add url for completed todos

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import get_page, del_page
+from .views import get_page, del_task, complete_task
 
 urlpatterns = [
     path('', get_page),
-    path('delete/<int:id>', del_page, name='delete_todo')
+    path('delete/<int:id>', del_task, name='delete_todo'),
+    path('completed/<int:id>', complete_task, name='completed_task')
 ]
