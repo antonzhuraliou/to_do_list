5c7a67911ef356b8f85c2e2828ca436806fe98e1
Anton <anton.zhurauliou@gmail.com>
2025-03-06 10:04:06 +0100

    Create a new Model which stores todos

--- tasks/models.py
+++ tasks/models.py
@@ -8,3 +8,7 @@ class Task(models.Model):
     is_completed = models.BooleanField(null=True, blank=True)
     created_at = models.DateField(null=True, blank=True)
 
+
+class CompletedTask(models.Model):
+    description = models.CharField(max_length=100)
+    created_at = models.DateField(null=True, blank=True)
