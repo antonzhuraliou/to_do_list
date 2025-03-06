e3a54746f025d92943b26c1679a6a12ad6e1a2e0
Anton <anton.zhurauliou@gmail.com>
2025-03-06 11:51:41 +0100

    Add a new URL for History TO-DO

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import get_page, del_task, complete_task
+from .views import get_page, del_task, complete_task, history_of_tasks
 
 urlpatterns = [
     path('', get_page),
     path('delete/<int:id>', del_task, name='delete_todo'),
-    path('completed/<int:id>', complete_task, name='completed_task')
+    path('completed/<int:id>', complete_task, name='completed_task'),
+    path('history/', history_of_tasks, name = 'history'),
 ]
