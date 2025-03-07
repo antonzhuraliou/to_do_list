7232270ca9e1c7cf76e849570672668d0f8b9c37
Anton <anton.zhurauliou@gmail.com>
2025-03-07 16:44:40 +0100

    Add path for edit buttong

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import get_page, del_task, complete_task, history_of_tasks
+from .views import get_page, del_task, complete_task, history_of_tasks, edit_task
 
 urlpatterns = [
     path('', get_page),
     path('delete/<int:id>', del_task, name='delete_todo'),
     path('completed/<int:id>', complete_task, name='completed_task'),
     path('history/', history_of_tasks, name = 'history'),
+    path('edit/<int:id>', edit_task, name='edit_todo')
 ]
