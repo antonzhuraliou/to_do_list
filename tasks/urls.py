975d1958bff496503f01071e89e9cc9e92734e7e
Anton <anton.zhurauliou@gmail.com>
2025-03-08 22:36:31 +0100

    Add url path for delete box

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
-from .views import get_page, del_task, complete_task, history_of_tasks, edit_task
+from .views import get_page, del_task, complete_task, history_of_tasks, edit_task, del_from_history
 
 urlpatterns = [
     path('', get_page),
     path('delete/<int:id>', del_task, name='delete_todo'),
     path('completed/<int:id>', complete_task, name='completed_task'),
     path('history/', history_of_tasks, name = 'history'),
-    path('edit/<int:id>', edit_task, name='edit_todo')
-]
+    path('edit/<int:id>/', edit_task, name='edit_todo'),
+    path('delete_history/<int:id>/', del_from_history, name = 'delete_history'),
+    ]
