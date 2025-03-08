cafc92e7e1140c23be617ccd378cdf47963c5192
Anton <anton.zhurauliou@gmail.com>
2025-03-08 23:15:08 +0100

    Add viwe for restore button

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
-from .views import get_page, del_task, complete_task, history_of_tasks, edit_task, del_from_history
+from .views import get_page, del_task, complete_task, history_of_tasks, edit_task, del_from_history, restore_todo
 
 urlpatterns = [
-    path('', get_page),
+    path('', get_page, name = 'main_page'),
     path('delete/<int:id>', del_task, name='delete_todo'),
     path('completed/<int:id>', complete_task, name='completed_task'),
     path('history/', history_of_tasks, name = 'history'),
     path('edit/<int:id>/', edit_task, name='edit_todo'),
     path('delete_history/<int:id>/', del_from_history, name = 'delete_history'),
+    path('restore_todo/<int:id>/', restore_todo, name = 'restore_todo'),
     ]
