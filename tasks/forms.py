94e58c4a05f77fe9a24b6ef547c85ef4456f814b
Anton <anton.zhurauliou@gmail.com>
2025-03-10 15:22:12 +0100

    Add a new form for edit button

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from .models import Task
 
 class TodoForm(forms.Form):
     add = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
         'class': 'form-control m-auto',
         'placeholder': 'Add a new todo...'
     }))
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['description']
+
+
