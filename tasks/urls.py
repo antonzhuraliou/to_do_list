bd2ca31021c08cb0c344be88a599d4efe58253a0
Anton <anton.zhurauliou@gmail.com>
2025-03-10 18:37:44 +0100

    Add url path fir edit history button

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import get_page, del_task, complete_task, history_of_tasks, edit_task, del_from_history, restore_todo
+from .views import get_page, del_task, complete_task, history_of_tasks, edit_task, del_from_history, restore_todo, edit_in_history
 
 urlpatterns = [
     path('', get_page, name = 'main_page'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('edit/<int:id>/', edit_task, name='edit_todo'),
     path('delete_history/<int:id>/', del_from_history, name = 'delete_history'),
     path('restore_todo/<int:id>/', restore_todo, name = 'restore_todo'),
+    path('edit_in_history/<int:id>/', edit_in_history, name='edit_history'),
     ]
