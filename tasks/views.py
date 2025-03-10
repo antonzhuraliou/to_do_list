8b349bea8b4a0dcc71f54b271352cffdb7f7ac8f
Anton <anton.zhurauliou@gmail.com>
2025-03-10 19:25:45 +0100

    Refactoring views

--- tasks/views.py
+++ tasks/views.py
@@ -1,72 +1,83 @@
-from django.shortcuts import render
-from .models import Task, CompletedTask
-from .forms import TodoForm
-from django.http import HttpResponseRedirect, HttpResponse
 from datetime import datetime
-from django.views.generic import UpdateView
-from .forms import EditForm
-import typing
+from typing import Any
 
-def get_page(request):
-    tasks = Task.objects.all()
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .models import Task, CompletedTask
+from .forms import TodoForm, EditForm
+
+
+def get_main_page(request):
+    all_tasks = Task.objects.all()
+    count_tasks = all_tasks.count()
     form = TodoForm()
-    count_tasks = Task.objects.all().count()
+
     if request.method == 'POST':
         form = TodoForm(request.POST)
         if form.is_valid():
-            new_tasks = Task.objects.create(description = form.cleaned_data['add'])
-            count_tasks += 1
+            Task.objects.create(description = form.cleaned_data['add'])
             return HttpResponseRedirect('/')
-    return render(request, 'tasks/cur_tasks.html', context = {'tasks': tasks, 'form': form, 'count': count_tasks})
 
-def del_task(request, id):
-    delete_task = Task.objects.get(id = id)
-    delete_task.delete()
-    return HttpResponseRedirect('/')
+    return render(request, 'tasks/cur_tasks.html', context = {'all_tasks': all_tasks, 'form': form, 'count_tasks': count_tasks})
+
 
 def complete_task(request, id):
-    delete_task = Task.objects.get(id=id)
-    completed_task = CompletedTask(description = delete_task.description, created_at = datetime.now())
-    delete_task.delete()
-    completed_task.save()
+    task_to_delete = Task.objects.get(id=id)
+    CompletedTask.objects.create(description = task_to_delete.description, created_at = datetime.now())
+    task_to_delete.delete()
     return HttpResponseRedirect('/')
 
 
-def history_of_tasks(request):
-    all_tasks = CompletedTask.objects.all()
-    create_tasks = CompletedTask.objects.values('created_at').distinct().order_by('-created_at', )
-    return render(request, 'tasks/history_page.html', context={'all_tasks': all_tasks, 'date': create_tasks})
+def delete_task(request, id):
+    task_to_delete = Task.objects.get(id = id)
+    task_to_delete.delete()
+    return HttpResponseRedirect('/')
+
 
 def edit_task(request, id):
-    cur_task = Task.objects.get(id = id)
+    current_task = Task.objects.get(id = id)
+
     if request.method == 'POST':
-        form = EditForm(request.POST, instance=cur_task)
+        form = EditForm(request.POST, instance=current_task)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/')
     else:
-        form = EditForm(instance=cur_task)
+        form = EditForm(instance=current_task)
+
     return render(request, 'tasks/edit_task.html', {'form': form})
 
-def del_from_history(request, id):
-    delete_task = CompletedTask.objects.get(id = id)
-    delete_task.delete()
+
+def task_history(request):
+    all_tasks = CompletedTask.objects.all()
+    distinct_dates = CompletedTask.objects.values('created_at').distinct().order_by('-created_at', )
+    return render(request, 'tasks/history_page.html', context={'all_tasks': all_tasks, 'date': distinct_dates})
+
+
+def delete_task_from_history(request, id):
+    task_to_delete = CompletedTask.objects.get(id = id)
+    task_to_delete.delete()
     return HttpResponseRedirect('/history')
 
-def restore_todo(request, id):
-    restore_task = CompletedTask.objects.get(id=id)
-    new_task = Task.objects.create(description = restore_task.description)
-    restore_task.delete()
+
+def restore_task_from_history(request, id):
+    task_to_restore = CompletedTask.objects.get(id=id)
+    Task.objects.create(description = task_to_restore.description)
+    task_to_restore.delete()
     return HttpResponseRedirect('/history')
 
 
-def edit_in_history(request, id):
-    cur_task = CompletedTask.objects.get(id = id)
+def edit_task_in_history(request, id):
+    current_task = CompletedTask.objects.get(id = id)
+
     if request.method == 'POST':
-        form = EditForm(request.POST, instance=cur_task)
+        form = EditForm(request.POST, instance=current_task)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/history')
+
     else:
-        form = EditForm(instance=cur_task)
+        form = EditForm(instance=current_task)
+
     return render(request, 'tasks/edit_task.html', {'form': form})
