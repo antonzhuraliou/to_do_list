af713fec44413ec15e7b4558f9972d61edf0d545
Anton <anton.zhurauliou@gmail.com>
2025-03-13 19:26:48 +0100

    Refactor name of form var

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Task
 
 class TodoForm(forms.Form):
-    add = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+    description = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
         'class': 'form-control m-auto',
         'placeholder': 'Add a new todo...'
     }))
