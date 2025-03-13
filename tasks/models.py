b17acd640bd597bf6cbf01260bcfb55e177ea0d7
Anton <anton.zhurauliou@gmail.com>
2025-03-13 19:27:36 +0100

    Change datetime to auto_now_add in created_at(Task)

--- tasks/models.py
+++ tasks/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
 
     description = models.CharField(max_length=100)
     is_completed = models.BooleanField(null=True, blank=True)
-    created_at = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CompletedTask(models.Model):
