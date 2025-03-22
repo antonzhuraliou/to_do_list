7ceb2baa120db9a6dc515cc68367cced804f8654
Anton <anton.zhurauliou@gmail.com>
2025-03-22 16:04:21 +0100

    Add paths for delete button and edit button for calendar days

--- tasks/urls.py
+++ tasks/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('task_for_day/<int:day>/<int:month>/<int:year>/', views.calendar_task, name = 'day_task'),
     path('task_for_day_past/<int:day>/<int:month>/<int:year>/', views.get_completed_tasks, name = 'completed_task'),
     path('change_month/<int:month>/<int:year>/<str:sign>', views.get_priv_or_next_calendar, name='change_month'),
+    path('delete_task_for_day/<int:id>/<int:day>/<int:month>/<int:year>/', views.delete_task_from_day, name='today_delete'),
+    path('edit_task_for_day/<int:id>/<int:day>/<int:month>/<int:year>/', views.edit_task_calendar, name = 'today_edit'),
     ]
