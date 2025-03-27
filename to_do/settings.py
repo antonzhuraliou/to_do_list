f0387c1a5b996d4524e9fcdc005cb95e58b2637c
Anton <anton.zhurauliou@gmail.com>
2025-03-27 15:21:28 +0100

    Connects PostgreSQL

--- to_do/settings.py
+++ to_do/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks'
+    'tasks',
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'to_do.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.getenv('DATABASE_NAME', 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'anton-zhurauliou',
+        'PASSWORD': '728272anton',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
