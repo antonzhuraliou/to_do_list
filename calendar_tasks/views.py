f9c107a8c2ae39767c9c3625ac9959bff57cb1e6
Anton <anton.zhurauliou@gmail.com>
2025-04-01 17:32:07 +0200

    Fix views for Back to calendar buttons

--- calendar_tasks/views.py
+++ calendar_tasks/views.py
@@ -17,11 +17,13 @@ def get_calendar(request):
     return render(request, 'calendar_tasks/calendar_page.html', context = {'days_in_month': days_in_month, 'empty_days': first_day_number, 'week_days':week_days,
                                                                   'today_year': today_date, 'actual_month': actual_month, 'today_compare': date.today()})
 
-def get_priv_or_next_calendar(request, month, year, sign):
+def get_priv_or_next_calendar(request, month, year, sign = ''):
     if sign == '+':
         current_month_date = date(year, month, 1) + relativedelta(months=1)
-    else:
+    elif sign == '-':
         current_month_date = date(year, month, 1) - relativedelta(months=1)
+    else:
+        current_month_date = date(year, month, 1)
     first_day_number, days_in_month = calendar.monthrange(current_month_date.year, current_month_date.month)
     actual_month = calendar.month_name[current_month_date.month]
     week_days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -39,14 +41,14 @@ def calendar_task(request, day, month, year):
             Task.objects.create(description=form.cleaned_data['description'], created_at = date(year, month, day))
             return HttpResponseRedirect(f'/calendar/task_for_day/{day}/{month}/{year}/')
 
-    return render(request, 'calendar_tasks/day_task_page.html', context = {'form': form, 'date_to_add':date_to_add, 'all_tasks': all_tasks})
+    return render(request, 'calendar_tasks/day_task_page.html', context = {'form': form, 'date_to_add':date_to_add, 'all_tasks': all_tasks, 'month': month})
 
 
 def get_completed_tasks(request, day, month, year):
     actual_month = calendar.month_name[month]
     date_to_add = (year, actual_month, day, month)
     completed_tasks = CompletedTask.objects.filter(created_at= date(year, month, day))
-    return render(request,  'calendar_tasks/completed_task.html', context = {'completed_tasks': completed_tasks, 'date_to_add': date_to_add})
+    return render(request,  'calendar_tasks/completed_task.html', context = {'completed_tasks': completed_tasks, 'date_to_add': date_to_add, 'month': month})
 
 
 def delete_task_from_day(request, id, day, month, year):
