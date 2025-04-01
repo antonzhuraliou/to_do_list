a481c17e78ef6af51d212a651be9be3f49e3e367
Anton <anton.zhurauliou@gmail.com>
2025-04-01 16:15:35 +0200

    Move calendar urls to calendar app

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from tasks import views
 
+app_name = 'tasks'
+
 urlpatterns = [
     path('', views.get_main_page, name = 'main_page'),
     path('delete/<int:id>', views.delete_task, name='delete_todo'),
@@ -11,10 +13,4 @@ urlpatterns = [
     path('delete_task_from_history/<int:id>/', views.delete_task_from_history, name = 'history_delete'),
     path('restore_task_from_history/<int:id>/', views.restore_task_from_history, name = 'history_restore'),
     path('edit_task_in_history/<int:id>/', views.edit_task_in_history, name='history_edit'),
-    path('get_calendar/', views.get_calendar, name='calendar'),
-    path('task_for_day/<int:day>/<int:month>/<int:year>/', views.calendar_task, name = 'day_task'),
-    path('task_for_day_past/<int:day>/<int:month>/<int:year>/', views.get_completed_tasks, name = 'completed_task'),
-    path('change_month/<int:month>/<int:year>/<str:sign>', views.get_priv_or_next_calendar, name='change_month'),
-    path('delete_task_for_day/<int:id>/<int:day>/<int:month>/<int:year>/', views.delete_task_from_day, name='today_delete'),
-    path('edit_task_for_day/<int:id>/<int:day>/<int:month>/<int:year>/', views.edit_task_calendar, name = 'today_edit'),
     ]
