c4d83447f22e0589311b047a68f8dc24a8aab90c
Anton <anton.zhurauliou@gmail.com>
2025-04-02 11:57:28 +0200

    Add url for search

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('complete/<int:id>', views.complete_task, name='complete_todo'),
     path('edit/<int:id>/', views.edit_task, name='edit_todo'),
     path('history/', views.task_history, name = 'history'),
-    path('delete_task_from_history/<int:id>/', views.delete_task_from_history, name = 'history_delete'),
-    path('restore_task_from_history/<int:id>/', views.restore_task_from_history, name = 'history_restore'),
-    path('edit_task_in_history/<int:id>/', views.edit_task_in_history, name='history_edit'),
+    path('delete_task_from_history/<int:id>/<str:query>', views.delete_task_from_history, name = 'history_delete'),
+    path('restore_task_from_history/<int:id>/<str:query>', views.restore_task_from_history, name = 'history_restore'),
+    path('edit_task_in_history/<int:id>/<str:query>', views.edit_task_in_history, name='history_edit'),
+    path('search/', views.search_history, name='search'),
     ]
