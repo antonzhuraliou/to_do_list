9c9560b20000a0977123cd0e0a2b6260c58291d7
Anton <anton.zhurauliou@gmail.com>
2025-04-04 12:12:34 +0200

    Create custom Login view and get_profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,39 @@
+from .forms import LoginForm
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
 
 def get_welcome_page(request):
     return render(request, 'accounts/welcome_page.html')
+
+
+def custom_login_view(request):
+    form = LoginForm
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user_obj = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user_obj = None
+
+        if user_obj:
+            user = authenticate(request, username=user_obj.username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("tasks:main_page")
+            else:
+                error = "Неверный пароль"
+        else:
+            error = "Пользователь с таким email не найден"
+
+        return render(request, "registration/login.html", {"error": error, 'form': form})
+    return render(request, "registration/login.html", {'form': form})
+
+
+def get_profile(request):
+    return render(request, 'accounts/profile.html')
