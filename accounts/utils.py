b49b6274751552628ab1231efc659f18afec02e7
Anton <anton.zhurauliou@gmail.com>
2025-04-19 13:43:18 +0000

    Create utils.py and make email_messages dict

---
+++ accounts/utils.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+
+
+email_messages = {
+    'after_registration': {'subject': 'Welcome! Your account has been created', 'message': f'Hi,\nThank you for registering with us! Your account has been successfully created. We are happy to have you on board!'},
+
+    'after_contact_us': {'subject': 'We’ve received your request', 'message': f'''Hello,
+    Thank you for reaching out to us. We’ve received your request and it will be reviewed shortly.
+    If you have any further questions or need assistance in the meantime, feel free to contact us at {settings.EMAIL_HOST_USER}.
+    Best regards,
+    [Your Company Name]'''},
+
+    'after_change_password': {'subject': 'Your Password Has Been Changed', 'message': f'''Hello,
+    We wanted to inform you that your password has been successfully changed.
+    If you did not request this change, please contact us immediately at {settings.EMAIL_HOST_USER}.
+    Best regards,
+    [Your Company Name]'''}
+}
