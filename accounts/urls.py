12495614437b036c43c1ed4d8ec30c73bb1c460f
Anton <anton.zhurauliou@gmail.com>
2025-04-19 15:00:30 +0000

    Add urls for delete_account

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('register/', views.get_register, name = 'register'),
     path('change_username/<str:change>/', views.change_username, name='change_username'),
     path('change-password/', views.change_password, name = 'change_password'),
-    path('contact_us/', views.contact_us, name='contact')
+    path('contact_us/', views.contact_us, name='contact'),
+    path('delete_account/', views.delete_account, name='delete_account')
 ]
