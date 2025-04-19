4dd176ead451a4a178bec563bf8f77fd0b49b2a5
Anton <anton.zhurauliou@gmail.com>
2025-04-19 13:45:03 +0000

    Create general func for send emails end use emails_messages

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ from .forms import ChangePasswordForm
 from django.contrib.auth import update_session_auth_hash
 from django.contrib import messages
 from django.core.mail import EmailMessage
+from .utils import email_messages
 
 
 def get_welcome_page(request):
@@ -55,11 +56,10 @@ def get_register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.cleaned_data['username']
             email = form.cleaned_data['email']
             form.save()
-            send_mail(subject='Welcome! Your account has been created',message=f'Hi {user},\nThank you for registering with us! Your account has been successfully created. We are happy to have you on board!',
-                      from_email=settings.DEFAULT_FROM_EMAIL, recipient_list=[email])
+            send_email_for_person(subject= email_messages['after_registration']['subject'],message= email_messages['after_registration']['message'],
+                      email=email)
             return render(request, 'accounts/success_registration.html')
     else:
         form = RegisterForm()
@@ -95,9 +95,12 @@ def change_password(request):
     if request.method == 'POST':
         form = ChangePasswordForm(user, request.POST)
         if form.is_valid():
+            email = request.user.email
             form.save()
             update_session_auth_hash(request, user)
             messages.add_message(request, messages.SUCCESS, 'Your Password Has Been Successfully Updated')
+            send_email_for_person(subject=email_messages['after_change_password']['subject'],
+             message=email_messages['after_change_password']['message'], email=email)
             return redirect('accounts:profile')
         else:
             return render(request, 'registration/password_change_form.html', context = {'form': form})
@@ -119,6 +122,7 @@ def contact_us(request):
             message = form.cleaned_data['message']
 
             send_contact_email_reply(subject, message, email)
+            send_email_for_person(subject=email_messages['after_contact_us']['subject'], message=email_messages['after_contact_us']['message'], email=email)
             messages.add_message(request, messages.SUCCESS, 'Your message has been received and is being processed. We will get back to you as soon as possible.')
 
             return redirect('accounts:profile')
@@ -135,3 +139,10 @@ def send_contact_email_reply(subject, message, email):
     )
     email.send(fail_silently=False)
 
+def send_email_for_person(subject, message, email):
+    send_mail(
+        subject = subject,
+        message = message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[email]
+    )
